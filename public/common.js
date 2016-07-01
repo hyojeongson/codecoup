602cde624d35c4275bbb8e95df5413889a6a67df
sow4063 <sow4063@gmail.com>
2016-07-01 20:55:00 +0900

    adding div to float

---
+++ public/common.js
@@ -0,0 +1,13 @@
+var boxLeft = $('box-left');
+var boxRight = $('box-right');
+
+boxLeft.css('background-color', 'orange');
+boxRight.css('background-color', 'lightgrey');
+
+boxLeft.css('float', 'left');
+boxRight.css('float', 'right');
+
+$('box-right').html('<h3>image rotating functions.</h3>');
+$('box-right').html('<input type="radio" name="circle" id="first"/>');
+$('box-right').html('<input type="radio" name="circle" id="second"/>');
+$('box-right').html('<input type="radio" name="circle" id="third"/>');
