f76dd2e2eb18cca1456f7f9dcb51ae609a23b90e
sow4063 <sow4063@gmail.com>
2016-07-01 21:24:51 +0900

    read

--- public/common.js
+++ public/common.js
@@ -1,5 +1,5 @@
-var boxLeft = $('box-left');
-var boxRight = $('box-right');
+var boxLeft = $('#box-left');
+var boxRight = $('#box-right');
 
 boxLeft.css('background-color', 'orange');
 boxRight.css('background-color', 'lightgrey');
@@ -7,7 +7,10 @@ boxRight.css('background-color', 'lightgrey');
 boxLeft.css('float', 'left');
 boxRight.css('float', 'right');
 
-$('box-right').html('<h3>image rotating functions.</h3>');
-$('box-right').html('<input type="radio" name="circle" id="first"/>');
-$('box-right').html('<input type="radio" name="circle" id="second"/>');
-$('box-right').html('<input type="radio" name="circle" id="third"/>');
+boxRight.html('<h3>image rotating functions.</h3>');
+boxRight.html('<input type="radio" name="circle" id="first"/>');
+boxRight.html('<input type="radio" name="circle" id="second"/>');
+boxRight.html('<input type="radio" name="circle" id="third"/>');
+
+$('#container').append(boxLeft);
+$('#container').append(boxRight);
